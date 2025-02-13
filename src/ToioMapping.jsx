8e33e7cc6b50b47545cbd51b4564413fba51f17a
Hyder Mohyuddin <hyder.mohyuddin@gmail.com>
2025-02-13 16:28:17 -0600

    got icons for the vehicles

--- src/ToioMapping.jsx
+++ src/ToioMapping.jsx
@@ -1,25 +1,35 @@
 import { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet/dist/images/marker-icon.png";
-import "leaflet/dist/images/marker-shadow.png";
 
 const vehicleIcons = {
   bike: L.icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/2015/2015415.png",
+    iconUrl: "/bike.png",
     iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+    className: "marker-outline",
   }),
   car: L.icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/741/741407.png",
+    iconUrl: "/car.png",
     iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+    className: "marker-outline",
   }),
   bus: L.icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/2418/2418779.png",
+    iconUrl: "/bus.png",
     iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+    className: "marker-outline",
   }),
   train: L.icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/1920/1920478.png",
+    iconUrl: "/train.png",
     iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+    className: "marker-outline",
   }),
 };
 
@@ -36,15 +46,17 @@ export default function ToioMapping() {
       }).addTo(map);
 
       const movingObjects = [
-        { id: 1, type: "bike", lat: 41.7923, lng: -87.5957 },
-        { id: 2, type: "car", lat: 41.7963, lng: -87.5897 },
-        { id: 3, type: "bus", lat: 41.7983, lng: -87.5857 },
-        { id: 4, type: "train", lat: 41.8003, lng: -87.5827 },
+        { id: 1, type: "bike", lat: 41.7923, lng: -87.5957, name: "Bike" },
+        { id: 2, type: "car", lat: 41.7963, lng: -87.5897, name: "Car" },
+        { id: 3, type: "bus", lat: 41.7983, lng: -87.5857, name: "Bus" },
+        { id: 4, type: "train", lat: 41.8003, lng: -87.5827, name: "Train" },
       ];
 
-      const markers = movingObjects.map((obj) =>
-        L.marker([obj.lat, obj.lng], { icon: vehicleIcons[obj.type] }).addTo(map)
-      );
+      movingObjects.forEach((obj) => {
+        L.marker([obj.lat, obj.lng], { icon: vehicleIcons[obj.type] })
+          .addTo(map)
+          .bindTooltip(obj.name, { permanent: false, direction: "top", offset: [0, -16] });
+      });
 
       return () => {
         map.remove();
@@ -52,5 +64,14 @@ export default function ToioMapping() {
     }
   }, []);
 
-  return <div id="map" style={{ width: "100vw", height: "100vh" }}></div>;
+  return (
+    <>
+      <style>{`
+        .marker-outline img {
+          filter: drop-shadow(0px 0px 3px black);
+        }
+      `}</style>
+      <div id="map" style={{ width: "100vw", height: "100vh" }}></div>
+    </>
+  );
 }
